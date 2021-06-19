51336cc5139bda3c1e677eb4fc5002ad79d0e2df
SAGOR <seakingmawmaw2020@gmail.com>
2021-06-19 18:09:05 +0600

    push the duplicate

--- debater.js
+++ debater.js
@@ -77,15 +77,146 @@
 // console.log(numberOfWords)
 
 
-const a = "mehdi hassan sagor i am a student of cse"
-let count = 1;
+// const a = "mehdi hassan sagor i am a student of cse"
+// let count = 1;
 
-for (let i = 0; i < a.length; i++) {
-  // console.log(i)
-  if(a.charAt(i) == ' '){
-    count ++;
+// for (let i = 0; i < a.length; i++) {
+//   // console.log(i)
+//   if(a.charAt(i) == ' '){
+//     count ++;
+//   }
+// }
+// console.log(count)
+
+// const str = "nazrul shuvo mehedi  hassan sagor"
+// let count = 1;
+// for (let i = 0; i < str.length; i++) {
+//   if(str.charAt(i) == ' '){
+//     count ++;
+//   }
+//
+// }
+// console.log(count)
+
+// const a = [12, 34, 45]
+// console.log(Math.min(...a))
+
+
+// var a = [12, 34, 45,67,89999]
+// let max = a[0];
+
+// for (let i = 0; i <a.length; i++) {
+//   if (a[i] < max){
+//     max = a[i]
+//   }
+// }
+// console.log(max)
+
+// let a = "i love teaching".split(" ")
+// let maxWord = a[0];
+// console.log(a)
+
+// for(let i = 1; i < a.length; i++)
+// if(a[i]>maxWord){
+//   maxWord = a[i];
+// }
+// console.log(maxWord)
+
+//kilometer to meter
+// function kilometerToMeter( kilometer ){
+//   //we know 1 km = 1000 m
+//   var meter = kilometer*1000;
+//   if( kilometer < 0 ){
+//   var negativeMeter = "distance cannot be negative"
+//   return negativeMeter;
+//   }
+//   else
+//   return meter;
+//   }
+//   var finalMeterIs = kilometerToMeter( 6);
+//   console.log(finalMeterIs);
+  //budget calculator
+  // function budgetCalculator(watch, phone,laptop ){
+  // var totalPrice = 0;
+  // if(watch * phone * laptop < 0){ //negative value here
+  // var forNegative = "element number cant be negative";
+  // return forNegative;
+  // }
+  // else
+  // var watchPrice = watch*50;
+  // var phonePrice = phone*100;
+  // var laptopPrice = laptop*500;
+  // var totalPrice = watchPrice + phonePrice + laptopPrice;
+  // return totalPrice;
+  // }
+  // var finalPrice = budgetCalculator(2,2,2);
+  // console.log(finalPrice);
+  //hotel cost
+   
+  // function hotelCost( day ){
+  // totalCost = 0;
+  // if( day < 0){
+  // var forNegativeDay = "day cant be negative"; //negative value here
+  // return forNegativeDay;
+  // }
+  // if(day <= 10){
+  // var first10 = day*100;
+  // var totalCost = totalCost+first10;
+  // }
+  // else if(day <= 20){
+  // var firstPart = 10*100;
+  // var rememberDay = day - 10;
+  // var second10 = rememberDay*80;
+  // var totalCost = firstPart + second10;
+  // }
+  // else{
+  // var firstPart = 10*100;
+  // var secondPart = 10*80;
+  // var rememberDay = day - 20;
+  // var thirdPart = rememberDay*50;
+  // var totalCost = firstPart + secondPart + thirdPart;
+  // }
+  // return totalCost;
+  // }
+  // var finalRent = hotelCost(40);
+  // console.log(finalRent);
+
+  // //large name
+  // function megaFriend (nameList){
+  // var nameLarge = 0;
+  // for (var i = 0; i < nameList.length; i++){
+  // if (nameList[i].length > nameLarge) {
+  // var nameLarge = nameList[i].length;
+  // var largest = nameList [i];
+  // }
+  // }
+  // return largest;
+  // }
+  // var largeName = megaFriend(["mehedi", "hassan", "nur islam","sobi run nesa","kajol rekha"]);
+  // console.log(largeName );
+
+
+//   let number = [1,2,3,4,5,3,2,3,4,5]
+
+// var uniqueNumber = []
+// for (let i = 0; i < number.length; i++) {
+//   const element = number[i];
+//   var index = uniqueNumber.indexOf(element)
+//  if (index === -1) {
+//    uniqueNumber.push(element)
+//  }
+// }
+// console.log(uniqueNumber)
+
+var speech = "i am a laday         killer man"
+
+var count = 0;
+for (let i = 0; i < speech.length; i++) {
+  const element = speech[i];
+  if (element == " " && speech[i-1] != " ") {
+    count++;
   }
+  
 }
+count++;
 console.log(count)
-
-
